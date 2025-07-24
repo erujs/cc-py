c01fd5a3c791885dba3457d82c903e0a2ae822b8
erujs <a.m.jerus@gmail.com>
2025-07-24 22:41:06 +0800

    minor fixes for templates

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,23 +2,25 @@ import os
 import shutil
 import sys
 
-def copy_template(template_name, target_name):
-    src = os.path.join(os.path.dirname(__file__), '..', template_name)
-    dest = os.path.join(os.getcwd(), target_name)
-
-    if not os.path.exists(src):
-        print(f"Error: Template '{template_name}' not found.")
+def copy_template(source_folder, destination_folder):
+    if not os.path.exists(source_folder):
+        print(f"ERROR: Source folder {source_folder} does not exist.")
         sys.exit(1)
 
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dest, item)
+    for item in os.listdir(source_folder):
+        s = os.path.join(source_folder, item)
+        d = os.path.join(destination_folder, item)
+
         if os.path.isdir(s):
-            shutil.copytree(s, d)
+            shutil.copytree(s, d, dirs_exist_ok=True)
         else:
             shutil.copy2(s, d)
 
-project_type = '{{ cookiecutter.project_type }}'
-project_slug = '{{ cookiecutter.project_slug }}'
+# Values from cookiecutter.json
+project_type = "{{ cookiecutter.project_type }}"
+project_slug = "{{ cookiecutter.project_slug }}"
 
-copy_template(project_type, project_slug)
+src = os.path.join(os.path.dirname(__file__), "..", project_type, project_slug)
+dest = os.path.join(os.getcwd(), project_slug)
+
+copy_template(src, dest)
